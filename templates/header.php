55ca1930c98597aed93a83bdd4fe60de448114c1
Fihaa <haikalwatampone3@gmail.com>
2022-01-06 13:03:56 +0800

    Added Chapter Admin

--- templates/header.php
+++ templates/header.php
@@ -393,7 +393,7 @@
                                                 <?php if (isset($_SESSION['l']))
                                                         if ($_SESSION['l'] == 1) { ?>
                                                         <li>
-                                                                <a <?php if ($title == "Admin | Perpustakaan Hakim" || $title == "User | Perpustakaan Hakim" || $title == "Book Admin | Perpustakaan Hakim") echo "class='active'" ?> href="#">
+                                                                <a <?php if ($title == "Admin | Perpustakaan Hakim" || $title == "User | Perpustakaan Hakim" || $title == "Book Admin | Perpustakaan Hakim" || $title == "Chapter | Perpustakaan Hakim") echo "class='active'" ?> href="#">
                                                                         <span class="nav-link-icon">
                                                                                 <i data-feather="copy"></i>
                                                                         </span>
@@ -408,6 +408,9 @@
                                                                         </li>
                                                                         <li>
                                                                                 <a href="a-book.php">Books</a>
+                                                                        </li>
+                                                                        <li>
+                                                                                <a href="chapter.php">Chapters</a>
                                                                         </li>
                                                                 </ul>
                                                         </li>
